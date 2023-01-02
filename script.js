9322323f7b8533fbe27fdce0da4f9164b4953de5
Vinícius Yoda <vyoda4604@gmail.com>
2023-01-02 17:01:01 -0300

    then catch finally

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-const consultaCep = fetch('https://viacep.com.br/ws/01001000/json/');
+const consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
+   .then(resposta => resposta.json())
+   .then(r => {
+      if (r.erro) {
+         throw Error('Esse cep não existe')
+      } else 
+         console.log(r)
+   })
+   .catch(erro => console.log(erro))
+   .finally(mensagem => console.log('Processamento concluído!'););
 
 console.log(consultaCep);
