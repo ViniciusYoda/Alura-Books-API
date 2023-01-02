07c8a8a16023ef3e863913a90a2978ef809f5ef3
Vinícius Yoda <vyoda4604@gmail.com>
2023-01-02 12:00:08 -0300

    inicando o projeto

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log('Mandando oi pro amigo')
+
+function mandaMensagem() {
+   console.log('Tudo bem');
+   console.log('Vou ');
+   console.log('Até');
+}
+
+mandaMensagem()
+
+console.log('tchau');
